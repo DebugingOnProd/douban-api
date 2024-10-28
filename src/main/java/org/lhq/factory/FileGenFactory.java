5eabce5b5d03d143b4c3d59834544a04499adcc3
DebuggingOnProd <lhqblue@outlook.com>
2024-10-28 23:17:01 +0800

    refactor(gen): 重构文件生成器实现
    - 将 GenJson 和 GenXml 类重命名为 GenBookInfoJson 和 GenBookInfoXml，以更清晰地表示它们的功能- 在 FileGenFactory 中添加私有构造函数，防止实例化工具类
    - 更新 FileGenFactory 中的 getFileGen 方法，使用新的生成器类
    - 删除了一些未使用的导入和注释，简化了代码结构

--- src/main/java/org/lhq/factory/FileGenFactory.java
+++ src/main/java/org/lhq/factory/FileGenFactory.java
@@ -2,20 +2,25 @@ package org.lhq.factory;
 
 import org.lhq.entity.book.BookInfo;
 import org.lhq.service.gen.Gen;
-import org.lhq.service.gen.impl.GenJson;
-import org.lhq.service.gen.impl.GenXml;
+import org.lhq.service.gen.impl.GenBookInfoJson;
+import org.lhq.service.gen.impl.GenBookInfoXml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FileGenFactory {
 
 
+    private FileGenFactory(){
+        throw new AssertionError("工具类不能实例化");
+    }
+
+
     private static final Logger log = LoggerFactory.getLogger(FileGenFactory.class);
 
     public static Gen<BookInfo> getFileGen(String fileType) {
         return switch (fileType.toLowerCase()) {
-            case "json" -> new GenJson();
-            case "xml" -> new GenXml();
+            case "json" -> new GenBookInfoJson();
+            case "xml" -> new GenBookInfoXml();
             default -> (dataItem, taskFile) -> log.warn("请实现文件写入生成器:{}", dataItem);
         };
     }
