833098409f5d66abedd27917434d87a93e80f068
DebuggingOnProd <lhqblue@outlook.com>
2024-10-29 23:50:07 +0800

    feat(task-scheduling): 更新定时任务执行策略
    - 将 fileListeningTask 的执行方式改为 scheduleAtFixedRate，初始延迟 5 秒，周期 100 秒
    - 保留 categorizedBookListeningTask 的执行方式，初始延迟 10 秒，周期 10 秒
    - 移除了未使用的日志记录代码块

--- src/main/java/org/lhq/init/AppLifecycleBean.java
+++ src/main/java/org/lhq/init/AppLifecycleBean.java
@@ -48,24 +48,14 @@ public class AppLifecycleBean {
         FileListening categorizedBookListeningTask = new CategorizedBookListening(beanUtils, dirConfigProperties);
         // 安排定时任务
         // 第一个参数是Runnable任务，第二个参数是首次执行的时间（延迟时间），第三个参数是周期时间，第四个参数是时间单位
-        //executor.schedule(fileListeningTask, 5, TimeUnit.SECONDS);
+        executor.scheduleAtFixedRate(fileListeningTask, 5, 100,TimeUnit.SECONDS);
         //这个任务定时扫描已经分类的书籍文件夹汇总成一个文件
         executor.scheduleAtFixedRate(categorizedBookListeningTask, 10,10, TimeUnit.SECONDS);
 
 
 
 
-      /*  executor.scheduleAtFixedRate(()->{
-            LOGGER.info("The application is running...");
-            ThreadPoolExecutor poolExecutor = ThreadPoolUtil.getExecutor(ThreadPoolType.FILE_RW_THREAD);
-            LOGGER.info("The current pool size: {}", poolExecutor.getPoolSize());
-            LOGGER.info("The current task queue size: {}", poolExecutor.getQueue().size());
-            LOGGER.info("The current active threads: {}", poolExecutor.getActiveCount());
-            LOGGER.info("The current completed tasks: {}", poolExecutor.getCompletedTaskCount());
-            LOGGER.info("The current largest pool size: {}", poolExecutor.getLargestPoolSize());
-            LOGGER.info("The current task count: {}", poolExecutor.getTaskCount());
-            LOGGER.info("---------------------------");
-        }, 0, 1, TimeUnit.SECONDS);*/
+
 
 
 
