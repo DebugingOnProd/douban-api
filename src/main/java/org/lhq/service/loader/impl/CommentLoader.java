5a9f444d0ea44af25c15e8a3a01f5ace1067428e
DebuggingOnProd <lhqblue@outlook.com>
2024-12-23 20:37:52 +0800

    refactor(service): 重构 CommentLoader 类
    - 添加必要的导入语句
    - 使 CommentLoader 类继承 EntityLoader<String>
    - 添加构造函数和 load 方法的骨架

--- src/main/java/org/lhq/service/loader/impl/CommentLoader.java
+++ src/main/java/org/lhq/service/loader/impl/CommentLoader.java
@@ -1,4 +1,16 @@
 package org.lhq.service.loader.impl;
 
-public class CommentLoader {
+import org.lhq.config.DoubanApiConfigProperties;
+import org.lhq.service.loader.EntityLoader;
+import org.lhq.service.perse.HtmlParseProvider;
+
+public class CommentLoader  extends EntityLoader<String> {
+    protected CommentLoader(DoubanApiConfigProperties doubanApiConfigProperties) {
+        super(doubanApiConfigProperties);
+    }
+
+    @Override
+    public String load(HtmlParseProvider<String> htmlParseProvider, String id) {
+        return "";
+    }
 }
