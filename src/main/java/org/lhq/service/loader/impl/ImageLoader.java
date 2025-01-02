e80358d4f039ad838ef2abf962dcfe5f6abb660d
DebuggingOnProd <lhqblue@outlook.com>
2025-01-02 23:29:34 +0800

    feat(imageLoader): 添加 cookie 认证以处理豆瓣相册爬取
    - 在 load 方法中添加了 cookie 认证，以解决豆瓣相册爬取时的登录验证问题
    - 引入了 DoubanUrlUtils 工具类来处理 cookie 的转换
    - 优化了网络请求的代码，增加了 cookies 参数

--- src/main/java/org/lhq/service/loader/impl/ImageLoader.java
+++ src/main/java/org/lhq/service/loader/impl/ImageLoader.java
@@ -7,12 +7,14 @@ import org.jsoup.Jsoup;
 import org.lhq.config.DoubanApiConfigProperties;
 import org.lhq.service.loader.EntityLoader;
 import org.lhq.service.perse.HtmlParseProvider;
+import org.lhq.service.utils.DoubanUrlUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Singleton
 @Named("imageLoader")
@@ -27,11 +29,14 @@ public class ImageLoader extends EntityLoader<List<Byte>> {
     public List<Byte> load(HtmlParseProvider<List<Byte>> htmlParseProvider, String imageUrl) {
         String baseUrl = doubanApiConfigProperties.baseUrl();
         String userAgent = doubanApiConfigProperties.userAgent();
+        String cookie = doubanApiConfigProperties.cookie();
+        Map<String, String> cookiesMap = DoubanUrlUtils.getCookies(cookie);
         try {
             Connection.Response response = Jsoup.connect(imageUrl)
                     .method(Connection.Method.GET)
                     .ignoreContentType(true)
                     .referrer(baseUrl)
+                    .cookies(cookiesMap)
                     .userAgent(userAgent)
                     .execute();
             if (response.statusCode() == 200) {
